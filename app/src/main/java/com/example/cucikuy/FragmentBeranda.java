fa27bae7effc36855af9d368800916fa19a52e4e
Ahmad <ahmad04102019@gmail.com>
2025-04-16 12:50:08 +0800

    fragment, but i wanna use navigation

---
+++ app/src/main/java/com/example/cucikuy/FragmentBeranda.java
@@ -0,0 +1,18 @@
+package com.example.cucikuy;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class FragmentBeranda extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_home, container, false);
+    }
+}
--- app/src/main/java/com/example/cucikuy/HomeActivity.java
+++ app/src/main/java/com/example/cucikuy/HomeActivity.java
@@ -1,78 +1,39 @@
 package com.example.cucikuy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button sign_out_btn;
-    FirebaseFirestore db;
-    FirebaseUser currentUser;
-
-    TextView tvEmail, tvNamaOutlet, tvAlamat, tvNoHp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        sign_out_btn = findViewById(R.id.sign_out_btn);
-        db = FirebaseFirestore.getInstance();
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        // Inisialisasi TextView dari layout
-        tvEmail = findViewById(R.id.tvEmail);
-        tvNamaOutlet = findViewById(R.id.nama_outlet);
-        tvAlamat = findViewById(R.id.alamat);
-        tvNoHp = findViewById(R.id.no_hp);
-        if (currentUser != null) {
-            String uid = currentUser.getUid();
-
-            db.collection("users").document(uid)
-                    .get()
-                    .addOnSuccessListener(documentSnapshot -> {
-                        if (documentSnapshot.exists()) {
-                            String email = currentUser.getEmail();
-                            String namaOutlet = documentSnapshot.getString("nama_outlet");
-                            String alamat = documentSnapshot.getString("alamat");
-                            String noHp = documentSnapshot.getString("no_hp");
-                            // Tampilkan komponen setelah datanya di-set
-                            tvEmail.setVisibility(View.VISIBLE);
-                            tvNamaOutlet.setVisibility(View.VISIBLE);
-                            tvAlamat.setVisibility(View.VISIBLE);
-                            tvNoHp.setVisibility(View.VISIBLE);
-
-                            // Tampilkan di TextView
-                            tvEmail.setText(email);
-                            tvNamaOutlet.setText(namaOutlet);
-                            tvAlamat.setText(alamat);
-                            tvNoHp.setText(noHp);
-
-                        } else {
-                            Toast.makeText(this, "Data tidak ditemukan", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnFailureListener(e -> {
-                        Toast.makeText(this, "Gagal ambil data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    });
-        }
-        sign_out_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(HomeActivity.this, MainActivity.class));
-                finish();
-            }
+        setContentView(R.layout.activity_for_fragment);
+        setupBottomNav();
+        loadFragment(new FragmentBeranda());
+    }
+    private void setupBottomNav() {
+        findViewById(R.id.homeOption).setOnClickListener(v -> {
+            loadFragment(new FragmentBeranda());
+        });
+        findViewById(R.id.orderOption).setOnClickListener(v -> {
+            loadFragment(new FragmentOrder());
+        });
+        findViewById(R.id.reportOption).setOnClickListener(v -> {
+            loadFragment(new FragmentReport());
+        });
+        findViewById(R.id.settingOption).setOnClickListener(v -> {
+            loadFragment(new FragmentSetting());
         });
     }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
 }
